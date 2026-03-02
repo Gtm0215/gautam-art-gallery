dd52d9763ff0c0b705951b191a63ff58ea4a525f
Gtm0215 <gautamlal0215@gmail.com>
2026-03-02 22:30:04 +0530

    Update Order.js

--- gautam-art-gallery/models/Order.js
+++ gautam-art-gallery/models/Order.js
@@ -2,16 +2,28 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
   items: [
     {
       painting: { type: mongoose.Schema.Types.ObjectId, ref: "Painting" },
       quantity: Number,
       size: String,
-      framed: String
+      framed: String,
+      price: Number
     }
   ],
-  total: Number,
-  createdAt: { type: Date, default: Date.now }
+
+  totalAmount: Number,
+
+  status: {
+    type: String,
+    default: "Pending"
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("Order", orderSchema);
