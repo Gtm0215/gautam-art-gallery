3ebebb3ca68d3c7f50759ea8ca4c47ac57f7fdbd
Gtm0215 <gautamlal0215@gmail.com>
2026-03-02 23:40:39 +0530

    Update server.js

--- gautam-art-gallery/server.js
+++ gautam-art-gallery/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
    STATIC FILES
 ================================= */
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 /* ===============================
    DATABASE CONNECTION
 ================================= */
