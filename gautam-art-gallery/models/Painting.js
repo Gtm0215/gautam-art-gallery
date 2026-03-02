74b7071769b5284a1dee1c067a0542d6b7a41f9e
Gtm0215 <gautamlal0215@gmail.com>
2026-03-02 19:29:29 +0530

    Update Painting.js

--- gautam-art-gallery/models/Painting.js
+++ gautam-art-gallery/models/Painting.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-const paintingSchema = new mongoose.Schema(
-  {
-    title: String,
-    description: String,
-    price: String,
-    image: String,
-    likes: { type: Number, default: 0 }
-  },
-  { timestamps: true }
-);
+const paintingSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  image: String,
+  stock: Number,
+  sizes: [String],
+  framedOptions: [String],
+  likes: { type: Number, default: 0 }
+});
 
 module.exports = mongoose.model("Painting", paintingSchema);
