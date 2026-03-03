da1e69bc5cb9ecdff51874e8cd28876808af1e86
Gtm0215 <gautamlal0215@gmail.com>
2026-03-03 12:34:18 +0530

    Update Painting.js

--- gautam-art-gallery/models/Painting.js
+++ gautam-art-gallery/models/Painting.js
@@ -6,7 +6,13 @@ const paintingSchema = new mongoose.Schema({
   price: Number,
   stock: Number,
   image: String,
-  likes: { type: Number, default: 0 }
+  likes: { type: Number, default: 0 },
+
+  // NEW FIELDS
+  isFeatured: { type: Boolean, default: false },
+  isHero: { type: Boolean, default: false },
+  section: { type: String, default: "featured" } 
+  // values: hero, explore, featured
 });
 
 module.exports = mongoose.model("Painting", paintingSchema);
